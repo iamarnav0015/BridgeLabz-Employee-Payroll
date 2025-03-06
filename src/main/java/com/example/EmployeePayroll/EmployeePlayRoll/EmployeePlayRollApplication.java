ef7bf960b168daaf8f42d867b0d2f63b10a22cdc
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-06 22:16:37 +0530

    [Arnav]Added initialize payroll setup

---
+++ src/main/java/com/example/EmployeePayroll/EmployeePlayRoll/EmployeePlayRollApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePayroll.EmployeePlayRoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePlayRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePlayRollApplication.class, args);
+	}
+
+}
